09c94893edb45a2b1b5c03b63a336c45dbb57934
Amanuel Anteneh <asa2rc@virginia.edu>
2025-11-12 20:02:42 -0500

    updates

--- notebooks/mem_comp.py
+++ notebooks/mem_comp.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.extend(["../", "../src"])
 from tqdm import tqdm
+import psutil
+import memory_profiler
 from memory_profiler import memory_usage
 import numpy as np
 from scipy.stats import poisson
@@ -24,7 +26,7 @@ plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 seed = 42  # for reproducibility
 th.manual_seed(seed)
 
-Ms = range(100, 700, 50)
+Ns = range(25, 100, 25)
 device = 'cpu'
 N = 25
 D = 2000
@@ -36,9 +38,10 @@ avg_fids_mosek = []
 hyperparams = Hyperparameters(lr=0.01, lr_decay=0.999, beta1=0.9, beta2=0.9, num_epochs=100, batch_size=25)
 
 
-for M in tqdm(Ms, desc="Performing memory experiments"):
+for N in tqdm(Ns, desc="Performing memory experiments"):
     # PNR detector POVM
-    true_povm = pnr_povm(hilbert_dim=M, N=N, eta=eta)
+    M = 608
+    true_povm = pnr_povm(hilbert_dim=M, N=M, eta=eta)
     true_povm = th.vstack( [th.diagonal(E) for E in true_povm] ).T
 
     max_avg = find_lambda_max(M, eps=1e-5)  # get max avg photon number for probes
@@ -58,12 +61,12 @@ for M in tqdm(Ms, desc="Performing memory experiments"):
 
     # compute mems
     base_line = memory_usage(timeout=1)[0]
-    mem = memory_usage( (learn_phase_insensitive_povm, (logits, hyperparams, dataset, lam_smoothing, False)), interval=0.01, max_usage=True, include_children=True)  
+    mem = memory_usage( (learn_phase_insensitive_povm, (logits, hyperparams, dataset, lam_smoothing, False)), interval=0.1, max_usage=True, include_children=not True)  
     mem_gd.append((mem - base_line) / 1000)  # mem is in Mb divide by 1k to get in Gb
 
     targets, probes = targets.cpu().numpy(), probes.cpu().numpy()
     base_line = memory_usage(timeout=1)[0]
-    mem = memory_usage( (phase_insensitive_loss_cvx, (targets, probes, lam_smoothing, "MOSEK")), interval=0.01, max_usage=True, include_children=True)
+    mem = memory_usage( (phase_insensitive_loss_cvx, (targets, probes, lam_smoothing, "MOSEK")), interval=0.1, max_usage=True, include_children=not True)
     mem_mosek.append((mem - base_line) / 1000)
 
 
