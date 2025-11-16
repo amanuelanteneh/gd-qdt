3a869bd8988ed425affc34fdb08891953435ebd5
Amanuel Anteneh <asa2rc@virginia.edu>
2025-11-16 15:11:48 -0500

    updates on legend

--- notebooks/mem_comp.py
+++ notebooks/mem_comp.py
@@ -16,7 +16,7 @@ from torch.utils.data import TensorDataset
 from src.loss import phase_insensitive_loss_cvx
 from src.gd_opt import learn_phase_insensitive_povm
 from src.quantum import pnr_povm
-from src.utils import find_lambda_max
+from src.utils import find_lambda_max, find_lambda_given_N, find_M_given_lambda
 from src.custom_types import Hyperparameters
 
 matplotlib.rcParams.update({"font.size": 16})
@@ -26,12 +26,12 @@ plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 seed = 42  # for reproducibility
 th.manual_seed(seed)
 
-Ns = range(25, 100, 25)
+Ns = [200] #range(25, 100, 25)
 device = 'cpu'
 N = 25
 D = 2000
-eta = 1.
-lam_smoothing = 0e-5
+eta = 0.85
+lam_smoothing = 1e-5
 mem_gd = []
 mem_mosek = []
 avg_fids_mosek = []
@@ -39,13 +39,16 @@ hyperparams = Hyperparameters(lr=0.01, lr_decay=0.999, beta1=0.9, beta2=0.9, num
 
 
 for N in tqdm(Ns, desc="Performing memory experiments"):
-    # PNR detector POVM
-    M = 608
-    true_povm = pnr_povm(hilbert_dim=M, N=M, eta=eta)
+    # Find max average photon number and corresponding M as in Liu et al. arXiv:2306.12622
+    max_avg = find_lambda_given_N(N=N, prob_threshold=0.9, iters=1000)
+    M = find_M_given_lambda(max_avg, 1e-5)
+    print(f"For N={N}, max avg photon number is {max_avg}, M={M}")
+    true_povm = pnr_povm(hilbert_dim=M, N=N, eta=eta)
     true_povm = th.vstack( [th.diagonal(E) for E in true_povm] ).T
 
-    max_avg = find_lambda_max(M, eps=1e-5)  # get max avg photon number for probes
-    n_bars = np.linspace(0, max_avg, D)
+    #max_avg = find_lambda_max(M, eps=1e-5)  # get max avg photon number for probes
+    n_bars = np.arange(1, max_avg, 1)  # Create probe states same as Liu et al. to make memory comparison fair
+    #n_bars = np.linspace(0, max_avg, D)
     probes = th.tensor(np.array([ poisson.pmf(k=list(range(M)), mu=n_bar) for n_bar in n_bars ]), dtype=th.float32) 
 
     targets = probes @ true_povm  
@@ -61,24 +64,26 @@ for N in tqdm(Ns, desc="Performing memory experiments"):
 
     # compute mems
     base_line = memory_usage(timeout=1)[0]
-    mem = memory_usage( (learn_phase_insensitive_povm, (logits, hyperparams, dataset, lam_smoothing, False)), interval=0.1, max_usage=True, include_children=not True)  
+    mem = memory_usage( (learn_phase_insensitive_povm, (logits, hyperparams, dataset, lam_smoothing, False)), interval=1, max_usage=True, include_children=True)  
+    print(f"Memory for Adam for N={N}, M={M}: {(mem - base_line)*2**20/10**9} GB")
     mem_gd.append((mem - base_line) / 1000)  # mem is in Mb divide by 1k to get in Gb
 
     targets, probes = targets.cpu().numpy(), probes.cpu().numpy()
     base_line = memory_usage(timeout=1)[0]
-    mem = memory_usage( (phase_insensitive_loss_cvx, (targets, probes, lam_smoothing, "MOSEK")), interval=0.1, max_usage=True, include_children=not True)
+    mem = memory_usage( (phase_insensitive_loss_cvx, (targets, probes, lam_smoothing, "MOSEK")), interval=1, max_usage=True, include_children=True)
+    print(f"Memory for MOSEK for N={N}, M={M}: {(mem - base_line)*2**20/10**9} GB")
     mem_mosek.append((mem - base_line) / 1000)
 
 
 
-plt.plot(Ms, mem_gd, label="Gradient Descent", marker="o")
-plt.plot(Ms, mem_mosek, label="CCO", marker="o")
-plt.xlabel(r"\textrm{Hilbert space dimension} $(M)$", fontsize=15)
-plt.ylabel(r"\textrm{Memory (GB)}", fontsize=15)
-# plt.yscale("log")
-plt.legend();
+# plt.plot(Ms, mem_gd, label=r'\textrm{Gradient Descent}', marker="o")
+# plt.plot(Ms, mem_mosek, label=r'\textrm{CCO}', marker="o")
+# plt.xlabel(r"\textrm{Hilbert space dimension} $(M)$", fontsize=15)
+# plt.ylabel(r"\textrm{Memory (GB)}", fontsize=15)
+# # plt.yscale("log")
+# plt.legend(fancybox=True, ncol=1, frameon=False);
 
-if eta < 1.0:
-    plt.savefig("figs/lossy/lossy_memory.png", dpi=300, bbox_inches='tight')
-else:
-    plt.savefig("figs/ideal/ideal_memory.png", dpi=300, bbox_inches='tight')
+# if eta < 1.0:
+#     plt.savefig("figs/lossy/lossy_memory.png", dpi=300, bbox_inches='tight')
+# else:
+#     plt.savefig("figs/ideal/ideal_memory.png", dpi=300, bbox_inches='tight')
--- src/utils.py
+++ src/utils.py
@@ -12,10 +12,10 @@ def unstack(tensor: th.Tensor, N: int, M: int) -> th.Tensor:
     return tensor.view(M, N, N)
 
 
-def find_lambda_max(M: int, eps: float = 1e-5):
+def find_lambda_max(M: int, eps: float = 1e-5, iters: int = 100) -> float:
     """
-    Find the largest lambda (coherent state average photon number) such that 1 - F(M-1; lambda) <= eps,
-    where F is the Poisson CDF.
+    Find the largest lambda (coherent state average photon number) such that 
+    1 - F(M-1; lambda) <= eps, where F is the Poisson CDF.
 
     Args:
         M: Fock-space cutoff (integer)
@@ -26,7 +26,7 @@ def find_lambda_max(M: int, eps: float = 1e-5):
     """
     low, hi = 0.0, float(M-1) 
 
-    for _ in range(100):
+    for _ in range(iters):
         mid = 0.5 * (low + hi)
         tail = 1 - poisson.cdf(M, mid)
         if tail > eps:
@@ -36,6 +36,50 @@ def find_lambda_max(M: int, eps: float = 1e-5):
 
     return low
 
+def find_M_given_lambda(lambda_avg: float, prob_threshold: float = 1e-5):
+    """
+    Find the smallest M (Fock-space cutoff) such that 
+    1 - F(M-1; lambda_avg) <= prob_threshold, where F is the Poisson CDF.
+    This gives the cutoff M for which the probability of measuring more than M-1 photons
+    is below the specified threshold.
+    Similar to what was done in arXiv:2306.12622.
+
+    Args:
+        lambda_avg: average photon number (float)
+        prob_threshold: tolerance (float)
+
+    Returns:
+        M (int)
+    """
+    M = 1
+    while True:
+        tail = 1 - poisson.cdf(M-1, lambda_avg)
+        if tail <= prob_threshold:
+            break
+        M += 1
+
+    return M
+
+def find_lambda_given_N(N: int, prob_threshold: float = 0.9, iters: int = 100):
+    """
+    Find the largest lambda (coherent state average photon number) such that 
+    1 - F(N; lambda) > prob_threshold, where F is the Poisson CDF.
+    This gives the lambda for which the probability of measuring more than N photons
+    is above the specified threshold.
+    Similar to what was done in arXiv:2306.12622.
+    """
+
+    low, hi = 0.0, float(2*N) 
+
+    for _ in range(iters):
+        mid = 0.5 * (low + hi)
+        tail = 1 - poisson.cdf(N, mid)
+        if tail > prob_threshold:
+            hi = mid
+        else:
+            low = mid
+
+    return low
 
 def circle_points(N: int, R: float = 1.0):
     """
